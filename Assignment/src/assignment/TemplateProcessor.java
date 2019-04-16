b61f1c50746f7e0b6e1c7545ce6eb6323f3e7b01
Mr-Brompton <mr.brompton@mail.com>
2019-04-16 16:20:02 +0100

    Merge with online version

---
+++ Assignment/src/assignment/TemplateProcessor.java
@@ -0,0 +1,7 @@
+package assignment;
+import java.util.Map;
+public interface TemplateProcessor {
+	void loadTemplate(String name, String template);
+	void loadTemplates(Map<String, String> templates);
+	String expandTemplate(String templateName, Map<String, Object> context);
+	}
